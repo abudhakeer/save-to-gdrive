9fdc5862ac54144fa30c6f440df0161e79644d9f
AbuAbdhullah <abu.cbe92@gmail.com>
2026-05-30 15:54:29 +0530

    Add permanent Chrome Web Store public key to manifest configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAue+ZB4XsqdhTKLZSeGPnK6RG0OTYf8B0i+kQfzxERV226ItlUJ/4pA6t12czzk983CDCFbCcrlUFO66yEbadBqXNDMAsSpUtTHl2zQChlaGhDHATvORPK+nYosgE9kOc2PzfRC/007+/Y5FEk6X0OaRuiI85BPrzGJa2vK5eCAWLkuSeOxmbsg+3xR34ROq50A/AARWi7ko4AQlguy3cMSICGFWyRVzVPk5nfeVc62T7R/nIVspHCRPYwQC+82UUhh01lJWT3GkjgvNl5/Ux/eevxvPU4uxrAyYC8LqQB3Wr5cgn+voAY3qlTEFnuzKJmwbWBT0A9Bdhxp8sm5Kx1QIDAQAB",
     name: "AirSave",
     description:
       "Save web assets, images, and documents directly to your Google Drive.",
@@ -13,9 +14,6 @@ export default defineConfig({
       client_id:
         "277746463787-6419q14fi1bfkvsv1cs97mtuh94o51d2.apps.googleusercontent.com",
       scopes: ["https://www.googleapis.com/auth/drive.file"],
-    },
-    manifest: {
-      key: "ancmikanfcngodakllchbicmkbgeclbn",
     },
     icons: {
       "16": "icon/16.png",
